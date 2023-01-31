ba71adf86805b6efc0744a8ef66e6b2a395f29c1
kilian-nagel <nagelkilian05@gmail.com>
2023-01-31 21:14:44 +0100

    refactor : modify index.tsx page , add css global styles and variables

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,24 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import * as React from 'react';
 function Navbar(){
     return(
         <header id="header" className='header'>
             <nav className='nav'>
-                <p><Link href="/home" className="logo">Nodes</Link></p>
+                <p><Link href="/home" className="logo">N_</Link></p>
                 <ul className="links">
                     <li className="item">
-                        <Link href="/home" className="link">Home</Link>
+                        <Link href="/home" className="link">home</Link>
                     </li>
                     <li className="item">
-                        <Link href="/Messages" className="link">Messages</Link>
+                        <Link href="/Messages" className="link">about</Link>
                     </li>
                     <li className="item">
-                        <Link href="" className="link">Settings</Link>
+                        <Link href="" className="link">contact</Link>
                     </li>
                 </ul>
+                <FontAwesomeIcon className="menu-btn" icon={['fas','bars-staggered']}/>
             </nav>           
         </header>
     )
