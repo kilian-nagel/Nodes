e24720f0f1fd511977f1ca15226256932ff72162
kilian-nagel <nagelkilian05@gmail.com>
2023-02-04 23:48:50 +0100

    refactor ( index.tsx ) : change html structure of hero section of the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Navbar from '@/components/navbar'
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBarsStaggered , faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faBarsStaggered, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 library.add(faBarsStaggered);
@@ -18,21 +18,95 @@ export default function Home() {
       </Head>
       <main className={inter.className}>
         <Navbar></Navbar>
-        <div id="homepage">
+        <section id="home">
+          <div className="inner">
+            <div className="content">
+              <h1 className="title">Join our awesome community</h1>
+              <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt aperiam consectetur provident tenetur pariatur.</p>
+              <div className="buttons">
+                <button className="btn-default cta">Sign up</button>
+                <button className="link-default cta">Login</button>
+              </div>
+            </div>
+            <div className="preview">
+              <img width={325} src="./home.jpg" alt="image d'un réseau social" />
+            </div>
+          </div>
+        </section>
+        <section id="partners">
+          <div className="inner">
+            <h2 className='subtitle'>We collaborate with awesome companies</h2>
+            <div className="slide">
+              <div className="row">
+                <span className="companies">Google Microsoft Mozilla NextJS Accenture Adobe Amazon Apple Atos&nbsp;</span>
+                <span className="companies">Google Microsoft Mozilla NextJS Accenture Adobe Amazon Apple Atos&nbsp;</span>
+              </div>
+              <div className="row">
+                <span className="companies">Capgemini Cisco Cognizant IBM Intel Mastercard Lenovo Oracle Qualcomm&nbsp;</span>
+                <span className="companies">Capgemini Cisco Cognizant IBM Intel Mastercard Lenovo Oracle Qualcomm&nbsp;</span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="features">
+          <div className="container">
+            <div className="inner flex-start-center">
+              <div className="count">
+                <p className="index flex-center-center">01</p>
+              </div>
+              <div className="content">
+                <h2 className="subtitle">Connect with your friends</h2>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur molestiae in sequi aut nisi quibusdam labore aliquid doloremque ratione quam perspiciatis, dignissimos aperiam velit natus consequatur dolore totam, nostrum esse!</p>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="inner flex-start-center">
+              <div className="count">
+                <p className="index flex-center-center">02</p>
+              </div>
+              <div className="content">
+                <h2 className="subtitle">Share your memories</h2>
+                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur molestiae in sequi aut nisi quibusdam labore aliquid doloremque ratione quam perspiciatis, dignissimos aperiam velit natus consequatur dolore totam, nostrum esse!</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+{/*<section id="homepage">
           <div className="content">
             <h1 className="title">Nodes - Social Network</h1>
-            <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt aperiam consectetur provident tenetur pariatur, iure impedit culpa temporibus nesciunt ad ipsam ex! Sapiente nesciunt culpa cumque, dolor quo a iure neque doloribus officiis esse.</p>
+            <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt aperiam consectetur provident tenetur pariatur.</p>
             <div className="buttons">
               <button className="btn-default cta">Sign up</button>
-              <FontAwesomeIcon className="menu-btn" icon={['fas','arrow-right']}/>
               <button className="link-default cta">Login</button>
             </div>
           </div>
           <div className="preview">
             <img width={400} src="./home.svg" alt="image d'un réseau social" />
           </div>
-        </div>
-      </main>
-    </>
-  )
-}
+        </section>
+        <section id="share" className='section'>
+          <div className="content">
+            <h2 className="title">Share your memories</h2>
+            <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, officiis similique fuga error eos quisquam non quam distinctio sapiente veritatis ullam deleniti repudiandae sit molestias blanditiis porro iste unde tenetur!</p>
+          </div>
+          <div className="preview">
+            <img width={400} src="./share-300.jpg"  alt="image de personnes" srcSet='./share-400.jpg 400w ./share-300.jpg 300w'
+            />
+          </div>
+        </section>
+        <section id="connect" className='section'>
+          <div className="content">
+            <h2 className="title">Connect with your friends</h2>
+            <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, officiis similique fuga error eos quisquam non quam distinctio sapiente veritatis ullam deleniti repudiandae sit molestias blanditiis porro iste unde tenetur!</p>
+          </div>
+          <div className="preview">
+            <img width={400} src="./connect.png"  alt="image de personnes" srcSet='./share-400.jpg 400w ./share-300.jpg 300w'
+            />
+          </div>
+</section>*/}
