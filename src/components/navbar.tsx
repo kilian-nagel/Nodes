7f5a5604bbe5fe27354eec6e13ac8e1fff9b9883
kilian-nagel <nagelkilian05@gmail.com>
2023-02-04 23:37:33 +0100

    refactor(navbar.tsx) : remove blur

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,29 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBarsStaggered , faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faBarsStaggered);
+
 import Link from 'next/link';
 import * as React from 'react';
 function Navbar(){
     return(
         <header id="header" className='header'>
             <nav className='nav'>
-                <p><Link href="/home" className="logo">N_</Link></p>
-                <ul className="links">
-                    <li className="item">
-                        <Link href="/home" className="link">home</Link>
-                    </li>
-                    <li className="item">
-                        <Link href="/Messages" className="link">about</Link>
-                    </li>
-                    <li className="item">
-                        <Link href="" className="link">contact</Link>
-                    </li>
-                </ul>
-                <FontAwesomeIcon className="menu-btn" icon={['fas','bars-staggered']}/>
+                <p><Link href="/home" className="logo">Nodes</Link></p>
+                <div className='right'>
+                    <ul className="links">
+                        <li className="item">
+                            <Link href="/home" className="link">Home</Link>
+                        </li>
+                        <li className="item">
+                            <Link href="/Messages" className="link">About</Link>
+                        </li>
+                        <li className="item">
+                            <Link href="" className="link">Contact</Link>
+                        </li>
+                    </ul>
+                    <button className='btn-default'>Sign up</button>
+                </div>
             </nav>           
         </header>
     )
