e622feb64fc2dfbd9386075e914d66d289e76601
kilian-nagel <nagelkilian05@gmail.com>
2023-02-06 23:50:01 +0100

    refactor ( navbar.tsx ) : format code

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,26 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import * as React from 'react';
-function Navbar(){
-    return(
+
+function Navbar() {
+    return (
         <header id="header" className='header'>
             <nav className='nav'>
-                <p><Link href="/home" className="logo">N_</Link></p>
-                <ul className="links">
-                    <li className="item">
-                        <Link href="/home" className="link">home</Link>
-                    </li>
-                    <li className="item">
-                        <Link href="/Messages" className="link">about</Link>
-                    </li>
-                    <li className="item">
-                        <Link href="" className="link">contact</Link>
-                    </li>
-                </ul>
-                <FontAwesomeIcon className="menu-btn" icon={['fas','bars-staggered']}/>
-            </nav>           
+                <p><Link href="/home" className="logo">Nodes</Link></p>
+                <div className="right">
+                    <ul className="links">
+                        <li className="item">
+                            <Link href="/home" className="link">home</Link>
+                        </li>
+                        <li className="item">
+                            <Link href="/Messages" className="link">about</Link>
+                        </li>
+                        <li className="item">
+                            <Link href="" className="link">contact</Link>
+                        </li>
+                    </ul>
+                    <button className='btn-default'>sign up</button>
+                </div>
+            </nav>
         </header>
     )
 }
