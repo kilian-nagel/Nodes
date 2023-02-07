99af168844fca78669f3cdb71666874b0667bb37
kilian-nagel <nagelkilian05@gmail.com>
2023-02-07 23:11:11 +0100

    feat ( app.tsx ) : add css import for footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import "../styles/navbar.css";
 import "../styles/global.css";
 import "../styles/reset.css";
 import "../styles/utility.css";
+import "../styles/footer.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
