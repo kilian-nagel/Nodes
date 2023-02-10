57a139e067089322d6bffff8cf6cf0c2cfd0ceb9
kilian-nagel <nagelkilian05@gmail.com>
2023-02-10 21:46:59 +0100

    Feat : add home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,21 @@
+
+import * as React from 'react';
+import { Component } from 'react';
+
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+import Feed from '@/components/feed';
+
+function Home() {
+    return ( 
+        <div id="home">
+            <Navbar></Navbar>
+            <main>
+                <Feed></Feed>
+            </main>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Home;
