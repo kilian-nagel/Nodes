8545ec1443869a8593e8faa79999948f88537ea9
kilian-nagel <nagelkilian05@gmail.com>
2023-02-11 21:17:13 +0100

    feat : add function to fetch and formats posts , add basic logic for feed component : it can renders posts and will fetch all the posts on the first render

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -1,18 +1,40 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { useState , useEffect } from 'react';
 import {posts} from "../data/posts";
+import {users} from "../data/users";
+import Navbar from './navbar';
 import Post from './post';
+import {user} from "../interfaces/user";
+import { postData } from '@/interfaces/post';
 
 const Feed:React.FunctionComponent = ()=>{
+    let [posts,setPosts] = useState<postData[]>([]);
+
+    useEffect(()=>{
+        setPosts(fetchPostsData());
+    },[])
+
     return ( 
         <div id="feed">
+            {posts.map((post,i)=><Post postContent={post.postContent} category={post.category} username={post.username} pictureUrl={post.pictureUrl}/>)}
         </div>
     );
 }
 
-function fetchPosts(){
-    Object.keys(posts).forEach(key=>{
+function fetchPostsData(){
+    let postsData:postData[] = [];
+    Object.keys(posts).forEach((_p,i)=>{
+        const post = posts[_p];
+        const user:user = users[post.source];
+        let postData = {
+            postContent:post.content,
+            category:post.category,
+            username:user.username,
+            pictureUrl:user.picture,
+        };
+        postsData.push(postData);
     });
+    return postsData;
 }
 
 export default Feed;
