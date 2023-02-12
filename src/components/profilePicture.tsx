9643d717fdf22ed31a7b4744da1e8775d2c14673
kilian-nagel <nagelkilian05@gmail.com>
2023-02-12 20:52:04 +0100

    feat ( profilePicture.tsx ) : add styling , add logic to display image only if there is a url

--- src/components/profilePicture.tsx
+++ src/components/profilePicture.tsx
@@ -7,9 +7,17 @@ interface props {
 }
 
 const ProfilePicture : React.FunctionComponent<props> = ({pictureUrl}) =>{
+    const style = {
+        borderRadius:"25px",
+        background:"var(--orochimaru)",
+        width:"50px",
+        height:"50px"
+    };
     return (
         <div className="profile-picture">
-            <img src={pictureUrl} alt="profile picture" />
+            {
+                pictureUrl ?  <img src={pictureUrl} style={style} alt="profile picture" /> : <div style={style}></div>
+            }
         </div>
     )
 }
