582b16858770070e0401feeaac8583a31dd592d3
kilian-nagel <nagelkilian05@gmail.com>
2023-02-12 20:48:25 +0100

    feat ( feed.tsx ) : add styling and a link to redirect to the post creation page

--- src/components/feed.tsx
+++ src/components/feed.tsx
@@ -6,6 +6,7 @@ import Navbar from './navbar';
 import Post from './post';
 import {user} from "../interfaces/user";
 import { postData } from '@/interfaces/post';
+import Link from 'next/link';
 
 const Feed:React.FunctionComponent = ()=>{
     let [posts,setPosts] = useState<postData[]>([]);
@@ -14,9 +15,34 @@ const Feed:React.FunctionComponent = ()=>{
         setPosts(fetchPostsData());
     },[])
 
+    const style = {
+        display:"flex",
+        alignItems:"flex-start",
+        flexDirection:"column",
+        gap:"var(--spacing-sm)",
+        maxWidth: "1250px",
+        padding:"0 var(--spacing-md)",
+        margin:"var(--spacing-lg) auto 0 auto"  
+    }
+
+    const buttonStyle = {
+        background:"black",
+        position:"absolute",
+        bottom:"2rem",
+        right:"2rem",
+        height:"50px",
+        width:"50px",
+        borderRadius:"50%",
+        fontSize:"20px",
+        color:"white"
+    }
+
     return ( 
-        <div id="feed">
+        <div id="feed" style={style}>
             {posts.map((post,i)=><Post postContent={post.postContent} category={post.category} username={post.username} pictureUrl={post.pictureUrl}/>)}
+            <Link href="./post" style={buttonStyle} className="newPost-btn">
+                +
+            </Link>
         </div>
     );
 }
