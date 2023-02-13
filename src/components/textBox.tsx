881b2fa841ed1276c3de8331ae10766b3e1ae356
kilian-nagel <nagelkilian05@gmail.com>
2023-02-13 21:27:24 +0100

    feat ( textBox.tsx ) : add logic to handle creation of a post ( get user input , parse input , input validation)

--- src/components/textBox.tsx
+++ src/components/textBox.tsx
@@ -1,20 +1,61 @@
+import { addPost } from '@/controllers/posts';
+import { userSchema } from '@/controllers/user';
 import * as React from 'react';
-import { Component } from 'react';
+
+interface post {
+    content:string,
+    category:string,
+    source:userSchema,
+    time:string
+}
+
+export function addPostToDatabase(){
+    let postCategory = "main"; // Temporary
+    let postContent = parsePostContent(getPostContent());
+    let post = createNewPost(postContent,postCategory);
+
+    if(isPostContentValid(postContent)){
+        addPost(post);
+    }
+}
+
+function isPostContentValid(content:string){
+    return content.length > 0 && content.length <= 300;
+}
+
+
+function parsePostContent(content:string){
+    content.trim();
+    return content;
+}
+
+function getPostContent(){
+    let textarea = document.querySelector("#textBox textarea") as HTMLTextAreaElement;
+    let content = textarea.value;
+    return content;
+}
+
+function createNewPost(content:string,category:string){
+    return {
+        content:content,
+        category:category,
+        source:{
+            uid:"-1",
+            username:"guest",
+            picture:"",
+            friends:[],
+            messages:[],
+            posts:[],
+        }, // Temporary.
+        time:new Date(),
+    }
+}
 
 const TextBox:React.FunctionComponent = () => {
     const style = {
         paddingTop:"var(--spacing-md)",
         maxWidth:"1250px",
         margin:"0 auto"
-    }
-
-    function createNewPost(content:string){
-    }
-
-    function getPostContent(){
-        let textarea = document.querySelector("textarea") as HTMLTextAreaElement;
-        let content = textarea.value;
-        return content;
     }
 
     return ( 
