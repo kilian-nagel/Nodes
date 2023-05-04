2d98d7c7495b0f5bbc9c75291fe9cc1ddf173a1f
kilian-nagel <nagelkilian05@gmail.com>
2023-05-04 12:31:47 +0200

    refactor : onclick events were not redirected correctly

--- src/components/postHeader.tsx
+++ src/components/postHeader.tsx
@@ -19,7 +19,7 @@ const PostHeader:React.FunctionComponent<props> = ({handleClick}) => {
     return ( 
         <header id="postHeader" className='flex-spaceBetween-center' style={style}>
             <Link href="../"  style={BackButtonstyle} className="back-btn" aria-label='go back' title='go back'>back</Link>
-            <Link href="./home" className='btn-default'  onClick={handleClick} aria-label='send the post' title='send the post'>send</Link>
+            <Link href="./home" className='btn-default'  onClick={()=>handleClick()} aria-label='send the post' title='send the post'>send</Link>
         </header>
     );
 }
