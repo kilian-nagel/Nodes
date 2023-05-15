018c2d0e8899a1b3ce4b3737804aaf01d6de7ece
kilian-nagel <nagelkilian05@gmail.com>
2023-05-15 17:44:48 +0200

    feat : add controller to add a user to db

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,16 +1,40 @@
-import { userSchema } from '@/models/users';
-import user from '../interfaces/user';
+
 import connectDb from '../lib/dbConnection';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getUserByName, getUsersByName } from '@/data/users';
 import userSchema from '@/interfaces/user';
+
 const userModel = require("../models/users");
 
-export interface userSchema {
-    uid:string,
-    username:string,
-    picture:string,
-    friends:Object[],
-    messages:Object[],
-    posts:Object[]
+/**
+ * Add a new user to the database
+ * 
+ * @param req - request made from the client-side, contains data of the post wich will be added.
+ * @param res
+ */
+export async function addUser(req:NextApiRequest,res:NextApiResponse){
+    const user:userSchema = req.body;
+    try {
+        let userExists:boolean = userModel.exists({name:user.username});
+        if(!userExists){
+            const newUser = new userModel({
+                uid:user.uid,
+                username:user.username,
+                picture:user.picture,
+                friends:user.friends,
+                messages:user.messages,
+                posts:user.posts
+            });
+            newUser.save();
+            res.status(201).end();
+        } else {
+            console.error("user already exists");
+            res.status(500).end();
+        }
+    } catch(err){
+        console.error(err);
+        res.status(500).end();
+    }
 }
 
 export async function getUsers(username:string){
