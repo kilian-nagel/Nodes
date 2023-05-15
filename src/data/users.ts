69a292f189782b07c9b788c3fd3e2bec615cde2f
kilian-nagel <nagelkilian05@gmail.com>
2023-05-15 17:42:29 +0200

    refactor  : extract database queries logic

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,24 +1,24 @@
+import userSchema from "@/interfaces/user";
+import { userModel } from "@/models/users";
 
-interface obj<T>{
-    [index:string]:T
+/**
+ * Get all users of the database that match the query passed in parameter
+ * 
+ * @param query
+ * @returns 
+ */
+export const getUsersByName = async (query:string):Promise<userSchema[]> => {
+    const users = await userModel.find({name:"\\"+query+"\\"}).exec().orFail(()=>console.error(new Error(`No user has a username that match the query : ${query}.`)));
+    return users;
 }
 
-interface user{
-    uid:number,
-    username:string,
-    picture:string,
-    friends:string[],
-    messages:string[],
-    posts:string[]
-}
-
-export const users:obj<user> = {
-    0:{
-        "uid":0,
-        "username":"guest",
-        "picture":"",
-        "friends":[],
-        "messages":[],
-        "posts":["0"]
-    }
+/**
+ * Get the user that has a username equal to the query passed in parameter
+ * 
+ * @param query 
+ * @returns 
+ */
+export const getUserByName = async (query:string):Promise<userSchema> => {
+    const user = await userModel.findOne({name:query}).exec().orFail(()=>console.error(new Error(`No user with the username : ${query} found.`)));
+    return user;
 }
