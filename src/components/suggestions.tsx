792e085bed38705276b78c7573d9c13a47d0700c
kilian-nagel <nagelkilian05@gmail.com>
2023-06-03 12:00:40 +0200

    feat : new component suggestions

---
+++ src/components/suggestions.tsx
@@ -0,0 +1,8 @@
+
+export const Suggestions:React.FunctionComponent = () => {
+    return (
+        <div id="suggestions">
+            <h2 className="title">Suggestions</h2>
+        </div>
+    )
+}
