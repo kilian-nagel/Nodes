5458f56aac2ba8959b94771f7656f29bb2f725c6
kilian-nagel <nagelkilian05@gmail.com>
2023-06-07 23:22:02 +0300

    feat : appearance and styles polished

--- src/components/trends.tsx
+++ src/components/trends.tsx
@@ -1,14 +1,14 @@
 
 export const Trends:React.FunctionComponent = () => {
     const style:React.CSSProperties = {
-        display:"flex",
-        flexDirection:"column",
-        gap:"var(--spacing-sm)"
+        gap:"var(--spacing-sm)",
+        marginTop:"var(--spacing-md)",
+        width:"300px"
     }
 
     return (
-        <div className="trends" id="trends" style={style}>
-            <h2 className="title">Trends</h2>
+        <div className="trends flex-start-start-column" id="trends" style={style}>
+            <h2 className="subtitle-2">Trends</h2>
             <div className="trend">
                 <h3>Vacations</h3>
                 <p className="text text-light">20k posts</p>
