f7044c34ad9096357b5a173f834810e51b7de1a6
kilian-nagel <nagelkilian05@gmail.com>
2023-06-07 23:18:31 +0300

    refactor : update css import links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0/client';  
-import "../styles/normalize.css";
-import "../styles/index.css";
-import "../styles/navbar.css";
-import "../styles/global.css";
-import "../styles/reset.css";
-import "../styles/utility.css";
-import "../styles/footer.css";
-import "../styles/home.css";
+import "../styles/rules/normalize.css";
+import "../styles/rules/global.css";
+import "../styles/rules/reset.css";
+import "../styles/components/navbar.css";
+import "../styles/components/footer.css";
+import "../styles/components/utility.css";
+import "../styles/pages/index.css";
+import "../styles/pages/home.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
