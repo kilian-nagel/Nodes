bfeee3b0f4c77b75e0159cd40d6bacd53d680777
kilian-nagel <nagelkilian05@gmail.com>
2023-06-07 23:30:28 +0300

    feat : change homePage layout : navbar is now aside and section for trends and suggestions added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 
-import Navbar from '@/components/navbar';
+import { AsideNavbar } from '@/components/asideNavbar';
 import Feed from '@/components/feed';
 import { Trends } from '@/components/trends';
 import { Suggestions } from '@/components/suggestions';
@@ -7,11 +7,15 @@ import { Suggestions } from '@/components/suggestions';
 function Home() {
     return ( 
         <div id="home">
-            <Navbar></Navbar>
             <main id='main'>
-                <Trends></Trends>
+                <aside className='left'>
+                    <AsideNavbar></AsideNavbar>
+                </aside>
                 <Feed></Feed>
-                <Suggestions></Suggestions>
+                <aside className="right">
+                    <Trends></Trends>
+                    <Suggestions></Suggestions>
+                </aside>
             </main>
         </div>
     );
