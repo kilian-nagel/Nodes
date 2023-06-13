6d98253bf63ab99424d55f0af791af343f7b0112
kilian-nagel <nagelkilian05@gmail.com>
2023-06-13 22:08:38 +0300

    feat : error handling for an axios request

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,12 +1,21 @@
+import { handleAxiosErrors } from "@/errors/axiosErrors";
 import postData from "@/interfaces/post";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 interface apiResponse {
     config:Object,
     data:postData[]
 }
 
-export const fetchPosts = async (query:string):Promise<apiResponse>=> {
-    const posts = await axios.get<any,apiResponse>(`/api/posts?query=${query}`);
-    return posts;
-}
+export const fetchPosts = async (query:string):Promise<apiResponse|undefined>=> {
+    try {
+        const posts = await axios.get<any, apiResponse>(`/api/posts?query=${query}`);
+        return posts;
+    } catch (err:unknown) {
+        if (axios.isAxiosError(err)) {
+          handleAxiosErrors(err);
+        } else if(err instanceof Error){
+          throw new Error("Unknown error - " + err.message);
+        }
+    }
+};
