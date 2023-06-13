3447bee880dc7f9fa2d806112485806e3edef623
kilian-nagel <nagelkilian05@gmail.com>
2023-06-13 18:10:18 +0300

    feat : add custom error - DbConnectionError

--- src/errors/utilityErrors.ts
+++ src/errors/utilityErrors.ts
@@ -6,6 +6,12 @@ export class dateError extends Error {
     }
 }
 
+export class DbConnectionError extends Error {
+    constructor(errorMessage:string){
+        super(errorMessage);
+    }
+}
+
 export function handleUtilityErrors(err:Error){
     if(err instanceof dateError){
         log("date error : "+err.message);
