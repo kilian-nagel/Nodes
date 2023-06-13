300999d2323b1f784a37eee3afb459f05f1b7ef2
kilian-nagel <nagelkilian05@gmail.com>
2023-06-13 17:48:19 +0300

    feat : add custom utility errors

---
+++ src/errors/utilityErrors.ts
@@ -0,0 +1,13 @@
+import { log } from "console";
+
+export class dateError extends Error {
+    constructor(errorMessage:string){
+        super(errorMessage);
+    }
+}
+
+export function handleUtilityErrors(err:Error){
+    if(err instanceof dateError){
+        log("date error : "+err.message);
+    }
+}
