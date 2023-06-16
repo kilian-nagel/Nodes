d2defb80babb9cbe95db33da05373a2425dea07b
kilian-nagel <nagelkilian05@gmail.com>
2023-06-16 18:14:28 +0300

    refactor : whitespace

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -3,19 +3,19 @@ import postData from "@/interfaces/post";
 import axios, { AxiosError } from "axios";
 
 interface apiResponse {
-    config:Object,
-    data:postData[]
+  config:Object,
+  data:postData[]
 }
 
 export const fetchPosts = async (query:string):Promise<apiResponse|undefined>=> {
-    try {
-        const posts = await axios.get<any, apiResponse>(`/api/posts?query=${query}`);
-        return posts;
-    } catch (err:unknown) {
-        if (axios.isAxiosError(err)) {
-          handleAxiosErrors(err);
-        } else if(err instanceof Error){
-          throw new Error("Unknown error - " + err.message);
-        }
+  try {
+    const posts = await axios.get<any, apiResponse>(`/api/posts?query=${query}`);
+    return posts;
+  } catch (err:unknown) {
+    if (axios.isAxiosError(err)) {
+      handleAxiosErrors(err);
+    } else if(err instanceof Error){
+      throw new Error("Unknown error - " + err.message);
     }
+  }
 };
