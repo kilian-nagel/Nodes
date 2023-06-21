2a0401c0e5522351d6d5c14000e16e8289aba624
kilian-nagel <nagelkilian05@gmail.com>
2023-06-21 20:07:37 +0300

    feat : navbars now display user profile if user is connected

--- src/components/asideNavbar.tsx
+++ src/components/asideNavbar.tsx
@@ -1,6 +1,14 @@
+
+
 import Link from "next/link"
+import { Profile } from "./profile/profile";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export const AsideNavbar:React.FunctionComponent = () => {
+    const {user} = useUser();
+    const username = user?.nickname;
+    const picture = user?.picture;
+
     const style = {
         width:"300px"
     }
@@ -10,8 +18,11 @@ export const AsideNavbar:React.FunctionComponent = () => {
     }
 
     return (
-        <div style={style}>
-            <h1 className="subtitle-2" style={titleStyle}><Link href="/">Nodes</Link></h1>
+        <div style={style}> 
+            {
+                user ? <Profile username={username?username:'guest'} pictureUrl={picture?picture:''}/> 
+                : <h1 className="subtitle-2" style={titleStyle}><Link href="/">Nodes</Link></h1>
+            }
             <nav className="asideNavbar" id="asideNavbar">
                 <ul>
                     <li><Link href="/" className="text-light">HOME</Link></li>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,20 @@
 import Link from 'next/link';
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { Profile } from './profile/profile';
+import { useEffect } from 'react';
 
 const Navbar:React.FunctionComponent = () => {
+    const { user, error, isLoading } = useUser();
+    if(error) console.log(error);
+    if(isLoading) console.log(isLoading);
+    
+    const username = user?.nickname;
+    const picture = user?.picture;
+
+    useEffect(()=>{
+        console.log(user);
+    },[user]);
+
     return (
         <header id="header" className='header'>
             <nav className='nav'>
@@ -17,7 +31,10 @@ const Navbar:React.FunctionComponent = () => {
                             <Link href="" className="link" aria-label="go to contact page" title='go to contact page'>contact</Link>
                         </li>
                     </ul>
-                    <Link href="/home" className='btn-default' aria-label='go to registration page' title='go to registration page'>sign up</Link>
+                    {
+                        user ? <Profile username={username ? username : 'guest'} pictureUrl={picture ? picture:''}/> 
+                        : <Link href="/auth" className='btn-default' aria-label='go to registration page' title='go to registration page'>sign up</Link> 
+                    }
                 </div>
             </nav>
         </header>
