4ce526219cd64a2b651e4ff45b6ea95f4da221aa
kilian-nagel <nagelkilian05@gmail.com>
2023-09-03 22:56:52 +0300

    feat : add handler for /users get requests

---
+++ src/pages/api/users.ts
@@ -0,0 +1,12 @@
+import { getUser } from "@/controllers/user";
+import { NextApiRequest, NextApiResponse } from "next";
+const l = console.log;
+
+export default async function handler(req:NextApiRequest,res:NextApiResponse){
+    switch(req.method){
+        case 'GET':{
+            getUser(req,res);
+            break;
+        }
+    }
+}
