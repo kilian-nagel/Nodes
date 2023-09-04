92c7f8e98419915381c4c8d71aaa69aa8b8704d2
kilian-nagel <nagelkilian05@gmail.com>
2023-09-04 19:38:18 +0300

    feat : add handling of post requests for /users path

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,4 +1,4 @@
-import { getUser } from "@/controllers/user";
+import { addUser, getUser } from "@/controllers/user";
 import { NextApiRequest, NextApiResponse } from "next";
 const l = console.log;
 
@@ -6,6 +6,10 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
     switch(req.method){
         case 'GET':{
             getUser(req,res);
+            break;
+        }
+        case 'POST':{
+            addUser(req,res);
             break;
         }
     }
