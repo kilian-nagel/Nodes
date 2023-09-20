409195ed0fb1fbc6c3ff065bdf9a990bcbc5bbc4
kilian-nagel <nagelkilian05@gmail.com>
2023-09-20 21:09:24 +0300

    refactor : handler now takes user uid in parameter

--- src/pages/postCreator.tsx
+++ src/pages/postCreator.tsx
@@ -6,17 +6,21 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { Suspense } from 'react';
 import { MoonLoader } from 'react-spinners';
 import { addPostToDatabase } from '@/data/posts';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
-function handleClickOnPostBtn(){
-    addPostToDatabase(getPostContent());
+function handleClickOnPostBtn(uid:string){
+    addPostToDatabase(getPostContent(),uid);
 }
 
 const Post:NextPage = () => {
+    const {user} = useUser();
+    const uid = user?.sub;
+
     return ( 
         <main>
             <ErrorBoundary fallback={<div>Could not load the page.</div>}>
                 <Suspense fallback={<MoonLoader/>}>
-                    <PostHeader handleClick={handleClickOnPostBtn}/>
+                    <PostHeader handleClick={()=>handleClickOnPostBtn(uid ? uid : "")}/>
                     <TextBox/>
                 </Suspense>
             </ErrorBoundary>
