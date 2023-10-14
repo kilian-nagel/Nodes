48c5c9ad0d3909ecb365fd653937483bc9017237
kilian-nagel <nagelkilian05@gmail.com>
2023-10-15 00:12:13 +0200

    refactor(components/post/dropdownMenu): keys are generated by randomUUID() api, memoization remove bevause useless

--- src/components/post/DropdownMenu.tsx
+++ src/components/post/DropdownMenu.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { useMemo } from "react";
-import { v4 as uuidv4 } from 'uuid';
 
 type option = {
   label:string,
@@ -12,8 +10,6 @@ interface props {
 }
 
 const DropdownMenu:React.FC<props> = ({ options }) => {
-    const uids = useMemo(()=> options.map(()=>uuidv4()),options);
-
     const style:React.CSSProperties = {
       position:'relative',
       padding:'0 0 0.75rem 0'
@@ -23,8 +19,8 @@ const DropdownMenu:React.FC<props> = ({ options }) => {
       <div className="dropdown-menu" style={style}>
         <button className="options-button">...</button>
         <ul className="options-list">
-          {options.map((option,index:number) => (
-            <li key={uids[index]}><Link className="text option" href={option.url}>{option.label}</Link></li>
+          {options.map((option) => (
+            <li key={crypto.randomUUID()}><Link className="text option" href={option.url}>{option.label}</Link></li>
           ))}
         </ul>
       </div>
