f8122ab51d50ad494f964698f7e66998c270a15a
kilian-nagel <nagelkilian05@gmail.com>
2023-10-22 23:58:26 +0200

    feat: add context for userData

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,52 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { UserProvider, useUser } from '@auth0/nextjs-auth0/client';
+import { createContext, useCallback, useEffect, useState } from 'react';
+import mongoose, { Mongoose } from 'mongoose';
+import userSchema from '@/interfaces/user';
+import { getUserInfo } from '@/data/users';
+
+export const UserDataContext = createContext({});
 
 export default function Document() {
+  const user = useUser();
+  const [userData,setUserData] = useState<userSchema>({
+    _id:new mongoose.Types.ObjectId("a"),
+    uid:"a",
+    username:"guest",
+    picture:"",
+    friends:[],
+    messages:[],
+    posts:[]
+  });
+
+  useEffect(()=>{
+    let flag = true;
+
+    async function fetchUserData(){
+      let uid;
+      if(user === undefined && user===null) return;
+      const data = await getUserInfo(userData.uid);
+      if(flag){
+        if(data===undefined){return;}
+        if(data.data !== undefined || data.data===null) return;
+        setUserData(data.data);
+      }   
+    }
+
+    return () => {
+      flag = false;
+    }
+  },[]);
+
   return (
     <Html lang="en">
       <Head />
       <body>
         <UserProvider>
-          <Main />
-          <NextScript />
+          <UserDataContext.Provider value={userData}>
+            <Main />
+            <NextScript />
+          </UserDataContext.Provider>
         </UserProvider>
       </body>
     </Html>
