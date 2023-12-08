a3e531594fb7d4b829dc9b1a14088bef1d71624e
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2023-12-08 23:10:16 +0200

    fix(pages/index.tsx): incorrect condition in useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       /* Si l'utilisateur avec l'uid actuelle n'existe pas cela signifie qu'il vient de crée son compte.
          Donc on demande au serveur de créer cet utilisateur.
       */
-      if(userData !== undefined && userData !== null && userData.data !== null && userData.data === "undefined"){
+      if(userData !== undefined && userData !== null && userData.data !== null && userData.data !== undefined){
         const nickname = user.nickname ? user.nickname : "guest";
         const userAdded = await addUser(nickname,user.sub);
         if (userAdded) await setCurrentUserData(user,setUserData);
