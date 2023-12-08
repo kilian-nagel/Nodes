917cc52ebc9a14b58687fcba88b899dea5cd93b5
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2023-12-08 22:59:29 +0200

    refactor(models/posts): better typing

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -39,7 +39,7 @@ export async function getRecentPostsFromDatabase():Promise<postDocument[]|undefi
     Modify the post that match the id of the post in given in argument.
     The post with this id will be updated to be equal to the argument. 
 */
-export async function modifyPostFromDatabase(postUpdated:postDocument){
+export async function modifyPostFromDatabase(postUpdated:postDocument):Promise<undefined>{
     await postModel.replaceOne({_id:postUpdated._id},postUpdated).catch((err:Error)=>{
         throw new modifyPostError(err.message);
     });
