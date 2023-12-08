0ab83bb19680f8c984db41d6c0d36fdc0dd74727
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2023-12-08 22:55:57 +0200

    refactor(api/users.ts): useless log

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,6 +1,5 @@
 import { addUser, getUser } from "@/controllers/user";
 import { NextApiRequest, NextApiResponse } from "next";
-const l = console.log;
 
 export default async function handler(req:NextApiRequest,res:NextApiResponse){
     switch(req.method){
