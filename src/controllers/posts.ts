56de4a19dd7750693b725b83cb0958eeac7167c6
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2023-12-08 23:06:23 +0200

    feat(controllers/posts.ts): delete post method

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -2,7 +2,7 @@
 import { NextApiRequest, NextApiResponse } from 'next/types';
 import { addPostError, getPostError, handlePostsErrors, postError } from '@/errors/postErrors';
 import { sanitizeMongoQuery } from '@/data/sanitize';
-import { addPostToDatabase, getRecentPostsFromDatabase, modifyPostFromDatabase } from '@/models/posts';
+import { addPostToDatabase, deleteFromDatabase, getRecentPostsFromDatabase, modifyPostFromDatabase } from '@/models/posts';
 import dbConnect from '@/lib/dbConnection';
 import { getUserByUid } from '@/models/users';
 
@@ -78,6 +78,35 @@ export async function modifyPost(req:NextApiRequest,res:NextApiResponse){
             console.error(err);
         }
         console.error(err);
+        res.status(500).end();
+    }
+}
+
+/**
+ * Modify the post that has the uid of the post passed in parameter. It will replace the old post ( the one in the database ) with the one passed in parameter
+ * 
+ * @param req
+ * @param res 
+**/
+export async function deletePost(req:NextApiRequest,res:NextApiResponse){
+    let idPost = req.body;
+    console.log(idPost);
+    try {
+        deleteFromDatabase(idPost);
+        res.status(200).end();
+    } catch(err:unknown){
+        if(err instanceof postError){
+            handlePostsErrors(err);
+        } else if(err instanceof Error) {
+            console.error(err);
+        }
+        console.error(err);
+        const response = {
+            success:false,
+            data:"failed to delete the post"
+        }
+
+        res.send(response);
         res.status(500).end();
     }
 }
