9606e46cdf87ce1006029050a447245abd8d8eea
kilian-nagel <kilian.nagel@etu.umontpellier.fr>
2023-12-24 12:14:32 +0200

    fix(repository/post.ts): cast string id to mongoose ObjectId

--- src/models/repository/post/MODIFY.ts
+++ src/models/repository/post/MODIFY.ts
@@ -1,12 +1,15 @@
 import { modifyPostError } from "@/errors/postErrors";
-import postModel, { postDocument } from "../../posts";
+import postModel from "../../posts";
+import mongoose from "mongoose";
+import { postSchemaPopulated } from "@/interfaces/post";
 
 /*
     Modify the post that match the id of the post in given in argument.
     The post with this id will be updated to be equal to the argument. 
 */
-export async function modifyPostFromDatabase(postUpdated:postDocument):Promise<undefined>{
-    await postModel.replaceOne({_id:postUpdated._id},postUpdated).catch((err:Error)=>{
+export async function modifyPostFromDatabase(postUpdated:postSchemaPopulated):Promise<undefined>{
+    const id = new mongoose.Types.ObjectId(postUpdated._id);
+    await postModel.replaceOne({_id:id},postUpdated).catch((err:Error)=>{
         throw new modifyPostError(err.message);
     });
 }
